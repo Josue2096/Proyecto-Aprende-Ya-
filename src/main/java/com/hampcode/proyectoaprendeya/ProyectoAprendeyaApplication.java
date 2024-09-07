dbef1ef27546c3594ba473dfda241ae576f78507
Josue2096 <62450028+Josue2096@users.noreply.github.com>
2024-09-07 09:26:25 -0500

    Conexion BD

---
+++ src/main/java/com/hampcode/proyectoaprendeya/ProyectoAprendeyaApplication.java
@@ -0,0 +1,13 @@
+package com.hampcode.proyectoaprendeya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAprendeyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoAprendeyaApplication.class, args);
+    }
+
+}
